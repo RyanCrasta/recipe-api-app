460a80bd45407366c7bc4ee1837331393bc25211
RyanCrasta <65001186+RyanCrasta@users.noreply.github.com>
2024-08-15 22:14:05 +0530

    Add railway app URL to CSRF trusted origin and allowed hosts

--- config/settings/development.py
+++ config/settings/development.py
@@ -2,7 +2,9 @@ from .base import *
 
 
 DEBUG = True
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'recipe-api-app-production.up.railway.app']
+
+CSRF_TRUSTED_ORIGIN = ['https://recipe-api-app-production.up.railway.app']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
