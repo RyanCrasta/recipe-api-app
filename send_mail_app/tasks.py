8e5480d7f214892b25eee60c29f1ed5bb453a92b
RyanCrasta <65001186+RyanCrasta@users.noreply.github.com>
2024-08-15 21:09:24 +0530

    send mail functionality

---
+++ send_mail_app/tasks.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import get_user_model
+from celery import shared_task
+from django.core.mail import send_mail
+from django.conf import settings
+
+@shared_task(bind=True)
+def send_mail_function(self):
+    users = get_user_model().objects.all()
+    for user in users:
+        mail_subject = 'Hello celery testing'
+        message = "assignment submission"
+        to_email = user.email
+        send_mail(subject=mail_subject, message=message, from_email=settings.EMAIL_HOST_USER,
+                  recipient_list=[to_email], fail_silently=True)
+
+    return 'MAIL SENT DONE'
