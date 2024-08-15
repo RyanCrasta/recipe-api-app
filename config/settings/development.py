72693d2ce3e306a5675b642950d94f75a9fa9d35
RyanCrasta <65001186+RyanCrasta@users.noreply.github.com>
2024-08-16 00:11:44 +0530

    Changing allowed hosts to *

--- config/settings/development.py
+++ config/settings/development.py
@@ -2,7 +2,7 @@ from .base import *
 
 
 DEBUG = True
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'recipe-api-app-production.up.railway.app']
+ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGIN = ['https://recipe-api-app-production.up.railway.app']
 
