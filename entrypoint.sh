1b6e5d87260fb743bcd5643d1ba56ae64903bd13
Ryan Crasta <65001186+RyanCrasta@users.noreply.github.com>
2024-08-18 18:15:47 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-set -e  # Exit immediately if a command exits with a non-zero status
-
-# Run Django database migrations
-python manage.py makemigrations --no-input
-python manage.py migrate --no-input
-
-# Collect static files (optional, if you're serving static files)
-python manage.py collectstatic --no-input
-
-# Start the Django server
-gunicorn config.wsgi
+python manage.py makemigrations
+python manage.py migrate
+python manage.py runserver 0.0.0.0:8000
