68c13e6c748b8fd84db5293b55a8748c3309d3ec
Ryan Crasta <65001186+RyanCrasta@users.noreply.github.com>
2024-08-18 16:30:36 +0530

    Make changes in entrypoint.sh to make it run in railway

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# Wait for PostgreSQL to be available
+until nc -z -v -w30 $POSTGRES_HOST 5432
+do
+  echo "Waiting for PostgreSQL database connection..."
+  sleep 1
+done
+
+# Run Django management commands
 python manage.py makemigrations
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+python manage.py collectstatic --noinput
+exec "$@"
